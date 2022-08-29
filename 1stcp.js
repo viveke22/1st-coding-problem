881028a41001f75a5e934c6bed8a8bca2478cfa0
Vivek <73058126+viveke22@users.noreply.github.com>
2022-08-29 15:04:37 +0530

    Add files via upload

---
+++ 1stcp.js
@@ -0,0 +1,31 @@
+function first(P1) {
+  if (P1 >= 10) {
+    return a = 1;
+  } else if (P1 < 10) {
+    return b = 0;
+  } 
+}
+function second(P2) {
+  if (P2 >= 10) {
+    return a = 1;
+  } else if (P2 < 10) {
+    return b = 0;
+  } 
+}
+function third(P3) {
+  if (P3 >= 10) {
+    return a = 1;
+  } else if (P3 < 10) {
+    return b = 0;
+  } 
+}
+function fourth(P4) {
+  if (P4 >= 10) {
+    return a = 1;
+  } else if (P4 < 10) {
+    return b = 0;
+  } 
+}
+console.log(first(30) + second(12)+third(35)+fourth(54));
+
+
